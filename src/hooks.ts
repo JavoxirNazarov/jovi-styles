ffffe699e5385edeb9a21c51eb3132e348a04ee7
javoxir97 <javohir.nazarov@incode-systems.com>
2026-04-16 12:08:04 +0500

    Fix quotation mark inconsistencies in hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,7 @@
-import { useContext, useMemo } from 'react';
-import type { ImageStyle, TextStyle, ViewStyle } from 'react-native';
-import type { ThemeContextValue } from './types';
-import { ThemeContext } from './context';
+import { useContext, useMemo } from "react";
+import type { ImageStyle, TextStyle, ViewStyle } from "react-native";
+import type { ThemeContextValue } from "./types";
+import { ThemeContext } from "./context";
 
 type StyleValue = ViewStyle | TextStyle | ImageStyle;
 type StyleFunction = (...args: any[]) => StyleValue;
@@ -15,7 +15,7 @@ export const useThemeContext = (): ThemeContextValue => {
   const context = useContext(ThemeContext);
 
   if (!context) {
-    throw new Error('useThemeContext must be used within a ThemeProvider');
+    throw new Error("useThemeContext must be used within a ThemeProvider");
   }
 
   return context;
